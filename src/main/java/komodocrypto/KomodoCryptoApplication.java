cfffcc6f3b742b17fa53a38958cdfc74d45984d9
hbwhyte <heather.b.whyte@gmail.com>
2018-04-27 14:57:55 +0800

    Init commit

---
+++ src/main/java/komodocrypto/KomodoCryptoApplication.java
@@ -0,0 +1,12 @@
+package co.codingnomads.bali2018.komodocrypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomodoCryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KomodoCryptoApplication.class, args);
+	}
+}
