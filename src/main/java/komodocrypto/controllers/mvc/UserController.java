20c813d86981a3d9577aaf06d79168a721b3a38c
Danica <Danicamaypriest@hotmail.co.uk>
2018-05-16 18:41:04 +0700

    working on controller

--- src/main/java/komodocrypto/controllers/mvc/UserController.java
+++ src/main/java/komodocrypto/controllers/mvc/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/komodo")
 public class UserController {
 
     @RequestMapping(value={"/login"}, method = RequestMethod.GET)
@@ -19,6 +20,13 @@ public class UserController {
     public ModelAndView home(){
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("home");
+        return modelAndView;
+    }
+    @RequestMapping(value={"/user"}, method = RequestMethod.GET)
+    public ModelAndView user(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("arbitrage", arbitrage);
+        modelAndView.setViewName("user_dashboard");
         return modelAndView;
     }
 }
