8a59982a0d88c7c48f2dc4d195bbe36040efa1b0
Danica <Danicamaypriest@hotmail.co.uk>
2018-05-18 09:48:36 +0700

    mapper and service updates

--- src/main/java/komodocrypto/mappers/ArbitrageMapper.java
+++ src/main/java/komodocrypto/mappers/ArbitrageMapper.java
@@ -1,8 +1,18 @@
 package komodocrypto.mappers;
 
+import komodocrypto.model.arbitrage.ArbitrageModel;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.ArrayList;
 
 @Mapper
 public interface ArbitrageMapper {
+
+    String GET_DATA = "SELECT * FROM `KomodoDB`.arbitrage";
+
+    @Select(GET_DATA)
+    public ArrayList<ArbitrageModel> getData();
     
 }
--- src/main/java/komodocrypto/services/arbitrage/ArbitrageService.java
+++ src/main/java/komodocrypto/services/arbitrage/ArbitrageService.java
@@ -1,4 +1,17 @@
 package komodocrypto.services.arbitrage;
 
+import komodocrypto.mappers.ArbitrageMapper;
+import komodocrypto.model.arbitrage.ArbitrageModel;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+
 public class ArbitrageService {
+    @Autowired
+    ArbitrageMapper arbitrageMapper;
+
+    public ArrayList<ArbitrageModel> getArbitrageData(){
+      ArrayList<ArbitrageModel> ad = arbitrageMapper.getData();
+      return ad;
+    };
 }
