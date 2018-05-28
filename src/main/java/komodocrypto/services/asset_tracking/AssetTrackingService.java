80de0791e88802d1ce20f710585809808df66323
keziaeuodia <setiadikezia@gmail.com>
2018-05-28 20:22:17 +0800

    binance dependency

--- src/main/java/komodocrypto/services/asset_tracking/AssetTrackingService.java
+++ src/main/java/komodocrypto/services/asset_tracking/AssetTrackingService.java
@@ -28,7 +28,7 @@ public class AssetTrackingService {
                     transferAmount = amount*price-exchangename.getTransfer_fee
          */
 
-        /* recalculate balance after buy/sell transaction
+       /* recalculate balance after buy/sell transaction
                 check balance on the exchange wallet of the selected currency
                 total1 = select total where exchange_id = exchange_name.getExchange_id and currency_id = currency_pair_id.getSymbol1
                 total2 = select total where exchange_id = exchange_name.getExchange_id and currency_id = currency_pair_id.getSymbol2
@@ -38,7 +38,7 @@ public class AssetTrackingService {
          */
 
         /* recalculate balance after transfer
-
+                will have to take in the 2 exchanges where the money is transferred from and to
          */
 
     }
@@ -46,6 +46,31 @@ public class AssetTrackingService {
     public void updateAssetsUnderManagement(int buy_exchange_id, int sell_exchange_id, Exchange exchange_name,
                                             double price, double amount, int signal, String status, int currency_pair_id){
 
+        /* transactionType determines buy, sell, or transfer
+                if buy
+                    buyAmount = amount*price-exchangename.getBuy_fee
+                if sell
+                    sellAmount = amount*price-exchangename.getSell_fee
+                if transfer
+                    transferAmount = amount*price-exchangename.getTransfer_fee
+         */
+
+         /* recalculate balance after buy/sell transaction
+                check balance on the exchange wallet of the selected currency
+                total1 = select total where exchange_id = exchange_name.getExchange_id and currency_id = currency_pair_id.getSymbol1
+                total2 = select total where exchange_id = exchange_name.getExchange_id and currency_id = currency_pair_id.getSymbol2
+                newTotal1 = total1 - sellAmount
+                newTotal2 = total2 + buyAmount
+
+         */
+
+         /* recalculate balance after transfer
+                will have to take in the 2 exchanges where the money is transferred from and to
+         */
+
+        /* parse new data to exchange wallet
+                not sure what frozen, borrowed, loaned, etc means
+         */
 
         /* update the balance of both currencies where the exchange happens on the arbitrage_trade_history table
                 insert currency_pair_id
@@ -59,13 +84,8 @@ public class AssetTrackingService {
                 insert sell_exchange_id
          */
 
-        /* update the exchange wallet - assuming this is what is accessed to check the balance of each currency?
-                insert currency_id
-                insert deposit_address
-                insert total
-         */
-
         /* update the balance in group_portfolio after transaction?
+                should there be a column for buy & sell amount here (considering there is a deposit column)?
                 current_value = getTokenAmount * usdExchangeRate
          */
     }
