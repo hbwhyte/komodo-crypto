3460475691d38a60a2778551879d0f47f288673a
Danica <Danicamaypriest@hotmail.co.uk>
2018-05-29 16:56:34 +0700

    deleted binace folder

--- binance-java-api/src/main/java/com/binance/api/client/domain/OrderSide.java
+++
@@ -1,9 +0,0 @@
-package com.binance.api.client.domain;
-
-/**
- * Buy/Sell order side.
- */
-public enum OrderSide {
-  BUY,
-  SELL
-}
--- binance-java-api/src/main/java/com/binance/api/client/domain/general/RateLimitType.java
+++
@@ -1,9 +0,0 @@
-package com.binance.api.client.domain.general;
-
-/**
- * Rate limiters.
- */
-public enum RateLimitType {
-  REQUESTS,
-  ORDERS
-}
